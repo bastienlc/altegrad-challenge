f34f45835e0fcc64c45f52be38f40480598f227b
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-12-13 15:34:13 +0100

    Format code

--- Model.py
+++ Model.py
@@ -1,8 +1,5 @@
 from torch import nn
-import torch.nn.functional as F
-
-from torch_geometric.nn import GCNConv
-from torch_geometric.nn import global_mean_pool
+from torch_geometric.nn import GCNConv, global_mean_pool
 from transformers import AutoModel
 
 
@@ -32,30 +29,36 @@ class GraphEncoder(nn.Module):
         x = self.mol_hidden1(x).relu()
         x = self.mol_hidden2(x)
         return x
-    
+
+
 class TextEncoder(nn.Module):
     def __init__(self, model_name):
         super(TextEncoder, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
-        
+
     def forward(self, input_ids, attention_mask):
         encoded_text = self.bert(input_ids, attention_mask=attention_mask)
-        #print(encoded_text.last_hidden_state.size())
-        return encoded_text.last_hidden_state[:,0,:]
-    
+        # print(encoded_text.last_hidden_state.size())
+        return encoded_text.last_hidden_state[:, 0, :]
+
+
 class Model(nn.Module):
-    def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels):
+    def __init__(
+        self, model_name, num_node_features, nout, nhid, graph_hidden_channels
+    ):
         super(Model, self).__init__()
-        self.graph_encoder = GraphEncoder(num_node_features, nout, nhid, graph_hidden_channels)
+        self.graph_encoder = GraphEncoder(
+            num_node_features, nout, nhid, graph_hidden_channels
+        )
         self.text_encoder = TextEncoder(model_name)
-        
+
     def forward(self, graph_batch, input_ids, attention_mask):
         graph_encoded = self.graph_encoder(graph_batch)
         text_encoded = self.text_encoder(input_ids, attention_mask)
         return graph_encoded, text_encoded
-    
+
     def get_text_encoder(self):
         return self.text_encoder
-    
+
     def get_graph_encoder(self):
         return self.graph_encoder
--- dataloader.py
+++ dataloader.py
@@ -1,21 +1,26 @@
 import os
 import os.path as osp
-import torch
-from torch_geometric.data import Dataset 
-from torch_geometric.data import Data
-from torch.utils.data import Dataset as TorchDataset
+
 import pandas as pd
+import torch
+from torch.utils.data import Dataset as TorchDataset
+from torch_geometric.data import Data, Dataset
+
 
 class GraphTextDataset(Dataset):
-    def __init__(self, root, gt, split, tokenizer=None, transform=None, pre_transform=None):
+    def __init__(
+        self, root, gt, split, tokenizer=None, transform=None, pre_transform=None
+    ):
         self.root = root
         self.gt = gt
         self.split = split
         self.tokenizer = tokenizer
-        self.description = pd.read_csv(os.path.join(self.root, split+'.tsv'), sep='\t', header=None)   
+        self.description = pd.read_csv(
+            os.path.join(self.root, split + ".tsv"), sep="\t", header=None
+        )
         self.description = self.description.set_index(0).to_dict()
         self.cids = list(self.description[1].keys())
-        
+
         self.idx_to_cid = {}
         i = 0
         for cid in self.cids:
@@ -29,75 +34,86 @@ class GraphTextDataset(Dataset):
 
     @property
     def processed_file_names(self):
-        return ['data_{}.pt'.format(cid) for cid in self.cids]
-    
+        return ["data_{}.pt".format(cid) for cid in self.cids]
+
     @property
     def raw_dir(self) -> str:
-        return osp.join(self.root, 'raw')
+        return osp.join(self.root, "raw")
 
     @property
     def processed_dir(self) -> str:
-        return osp.join(self.root, 'processed/', self.split)
+        return osp.join(self.root, "processed/", self.split)
 
     def download(self):
         pass
-        
+
     def process_graph(self, raw_path):
-      edge_index  = []
-      x = []
-      with open(raw_path, 'r') as f:
-        next(f)
-        for line in f: 
-          if line != "\n":
-            edge = *map(int, line.split()), 
-            edge_index.append(edge)
-          else:
-            break
-        next(f)
-        for line in f: #get mol2vec features:
-          substruct_id = line.strip().split()[-1]
-          if substruct_id in self.gt.keys():
-            x.append(self.gt[substruct_id])
-          else:
-            x.append(self.gt['UNK'])
-        return torch.LongTensor(edge_index).T, torch.FloatTensor(x)
+        edge_index = []
+        x = []
+        with open(raw_path, "r") as f:
+            next(f)
+            for line in f:
+                if line != "\n":
+                    edge = (*map(int, line.split()),)
+                    edge_index.append(edge)
+                else:
+                    break
+            next(f)
+            for line in f:  # get mol2vec features:
+                substruct_id = line.strip().split()[-1]
+                if substruct_id in self.gt.keys():
+                    x.append(self.gt[substruct_id])
+                else:
+                    x.append(self.gt["UNK"])
+            return torch.LongTensor(edge_index).T, torch.FloatTensor(x)
 
     def process(self):
-        i = 0        
+        i = 0
         for raw_path in self.raw_paths:
-            cid = int(raw_path.split('/')[-1][:-6])
-            text_input = self.tokenizer([self.description[1][cid]],
-                                   return_tensors="pt", 
-                                   truncation=True, 
-                                   max_length=256,
-                                   padding="max_length",
-                                   add_special_tokens=True,)
+            cid = int(raw_path.split("/")[-1][:-6])
+            text_input = self.tokenizer(
+                [self.description[1][cid]],
+                return_tensors="pt",
+                truncation=True,
+                max_length=256,
+                padding="max_length",
+                add_special_tokens=True,
+            )
             edge_index, x = self.process_graph(raw_path)
-            data = Data(x=x, edge_index=edge_index, input_ids=text_input['input_ids'], attention_mask=text_input['attention_mask'])
+            data = Data(
+                x=x,
+                edge_index=edge_index,
+                input_ids=text_input["input_ids"],
+                attention_mask=text_input["attention_mask"],
+            )
 
-            torch.save(data, osp.join(self.processed_dir, 'data_{}.pt'.format(cid)))
+            torch.save(data, osp.join(self.processed_dir, "data_{}.pt".format(cid)))
             i += 1
 
     def len(self):
         return len(self.processed_file_names)
 
     def get(self, idx):
-        data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(self.idx_to_cid[idx])))
+        data = torch.load(
+            osp.join(self.processed_dir, "data_{}.pt".format(self.idx_to_cid[idx]))
+        )
         return data
 
     def get_cid(self, cid):
-        data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(cid)))
+        data = torch.load(osp.join(self.processed_dir, "data_{}.pt".format(cid)))
         return data
-    
-    
+
+
 class GraphDataset(Dataset):
     def __init__(self, root, gt, split, transform=None, pre_transform=None):
         self.root = root
         self.gt = gt
         self.split = split
-        self.description = pd.read_csv(os.path.join(self.root, split+'.txt'), sep='\t', header=None)
+        self.description = pd.read_csv(
+            os.path.join(self.root, split + ".txt"), sep="\t", header=None
+        )
         self.cids = self.description[0].tolist()
-        
+
         self.idx_to_cid = {}
         i = 0
         for cid in self.cids:
@@ -111,62 +127,65 @@ class GraphDataset(Dataset):
 
     @property
     def processed_file_names(self):
-        return ['data_{}.pt'.format(cid) for cid in self.cids]
-    
+        return ["data_{}.pt".format(cid) for cid in self.cids]
+
     @property
     def raw_dir(self) -> str:
-        return osp.join(self.root, 'raw')
+        return osp.join(self.root, "raw")
 
     @property
     def processed_dir(self) -> str:
-        return osp.join(self.root, 'processed/', self.split)
+        return osp.join(self.root, "processed/", self.split)
 
     def download(self):
         pass
-        
+
     def process_graph(self, raw_path):
-      edge_index  = []
-      x = []
-      with open(raw_path, 'r') as f:
-        next(f)
-        for line in f: 
-          if line != "\n":
-            edge = *map(int, line.split()), 
-            edge_index.append(edge)
-          else:
-            break
-        next(f)
-        for line in f:
-          substruct_id = line.strip().split()[-1]
-          if substruct_id in self.gt.keys():
-            x.append(self.gt[substruct_id])
-          else:
-            x.append(self.gt['UNK'])
-        return torch.LongTensor(edge_index).T, torch.FloatTensor(x)
+        edge_index = []
+        x = []
+        with open(raw_path, "r") as f:
+            next(f)
+            for line in f:
+                if line != "\n":
+                    edge = (*map(int, line.split()),)
+                    edge_index.append(edge)
+                else:
+                    break
+            next(f)
+            for line in f:
+                substruct_id = line.strip().split()[-1]
+                if substruct_id in self.gt.keys():
+                    x.append(self.gt[substruct_id])
+                else:
+                    x.append(self.gt["UNK"])
+            return torch.LongTensor(edge_index).T, torch.FloatTensor(x)
 
     def process(self):
-        i = 0        
+        i = 0
         for raw_path in self.raw_paths:
-            cid = int(raw_path.split('/')[-1][:-6])
+            cid = int(raw_path.split("/")[-1][:-6])
             edge_index, x = self.process_graph(raw_path)
             data = Data(x=x, edge_index=edge_index)
-            torch.save(data, osp.join(self.processed_dir, 'data_{}.pt'.format(cid)))
+            torch.save(data, osp.join(self.processed_dir, "data_{}.pt".format(cid)))
             i += 1
 
     def len(self):
         return len(self.processed_file_names)
 
     def get(self, idx):
-        data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(self.idx_to_cid[idx])))
+        data = torch.load(
+            osp.join(self.processed_dir, "data_{}.pt".format(self.idx_to_cid[idx]))
+        )
         return data
 
     def get_cid(self, cid):
-        data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(cid)))
+        data = torch.load(osp.join(self.processed_dir, "data_{}.pt".format(cid)))
         return data
-    
+
     def get_idx_to_cid(self):
         return self.idx_to_cid
-    
+
+
 class TextDataset(TorchDataset):
     def __init__(self, file_path, tokenizer, max_length=256):
         self.tokenizer = tokenizer
@@ -174,7 +193,7 @@ class TextDataset(TorchDataset):
         self.sentences = self.load_sentences(file_path)
 
     def load_sentences(self, file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             lines = file.readlines()
         return [line.strip() for line in lines]
 
@@ -188,12 +207,12 @@ class TextDataset(TorchDataset):
             sentence,
             add_special_tokens=True,
             max_length=self.max_length,
-            padding='max_length',
+            padding="max_length",
             truncation=True,
-            return_tensors='pt'
+            return_tensors="pt",
         )
 
         return {
-            'input_ids': encoding['input_ids'].squeeze(),
-            'attention_mask': encoding['attention_mask'].squeeze()
+            "input_ids": encoding["input_ids"].squeeze(),
+            "attention_mask": encoding["attention_mask"].squeeze(),
         }
