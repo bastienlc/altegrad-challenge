b9e4a21a5bc4632379dc4f30a56b8e9287b2f5e0
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-01-16 18:51:23 +0100

    Add train.py script to run distant gpu

---
+++ train.py
@@ -0,0 +1,35 @@
+import torch
+from torch import optim
+from transformers import AutoTokenizer
+
+from load import load_dataset
+from models.baseline import BaselineModel
+from training_utils import train
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_name = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+train_loader, val_loader = load_dataset(tokenizer)
+
+model = BaselineModel(
+    model_name=model_name,
+    num_node_features=300,
+    nout=768,
+    nhid=300,
+    graph_hidden_channels=450,
+).to(device)
+
+optimizer = optim.AdamW(
+    model.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=0.01
+)
+
+save_path, _, _ = train(
+    model,
+    optimizer,
+    train_loader,
+    val_loader,
+    nb_epochs=10,
+    device=device,
+    load_from="outputs/saved/model5.pt",
+)
