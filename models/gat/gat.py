b06a6eaff8f6804fa862278fd65a834d1f9f5c66
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-01-18 14:43:40 +0100

    Fix hyperparameters for gat

--- models/gat/gat.py
+++ models/gat/gat.py
@@ -11,7 +11,7 @@ class GATEncoder(nn.Module):
         super(GATEncoder, self).__init__()
         self.nhid = nhid
         self.nout = nout
-        self.gat = GAT(num_node_features, nhid, graph_hidden_channels, 0.8, 0.2, 3)
+        self.gat = GAT(num_node_features, nhid, graph_hidden_channels, 0.1, 0.01, 10)
         self.mol_hidden1 = nn.Linear(graph_hidden_channels, nhid)
         self.mol_hidden2 = nn.Linear(nhid, nout)
 
