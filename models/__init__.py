a431a0db121cdc541f46ef4c5f1c13d2fc01200e
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-01-23 14:17:16 +0100

    Add a small GIN model

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .baseline import BaselineModel
+from .deepgat import DeepGATModel
+from .gat import GATModel
+from .gin import GINModel
+from .graphormer import GraphormerModel
+from .graphsage import SAGEModel
+from .pathnn import PathNNModel
