a10a2709edda508ce8dc7045bc6705d1d5994ba3
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-01-23 21:59:03 +0100

    Remove problematic init.py for kaggle

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .baseline import BaselineModel
-from .deepgat import DeepGATModel
-from .gat import GATModel
-from .gin import GINModel
-from .graphormer import GraphormerModel
-from .graphsage import SAGEModel
-from .pathnn import PathNNModel
