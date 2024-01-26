19287845c80a492084a54b96638f069c44f27459
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-01-26 19:47:51 +0100

    Add scheduler and different loss

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from torch import optim
 from transformers import AutoTokenizer
 
 from load import load_dataset
-from models.deepgat import DeepGATModel
+from models.torch_gat import GATModel
 from utils import train
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -13,10 +13,14 @@ embeddings_dim = 384
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 train_loader, val_loader = load_dataset(tokenizer)
 
-model = DeepGATModel(
+model = GATModel(
     model_name=model_name,
     num_node_features=300,
     nout=embeddings_dim,
+    d_hidden_dim=1000,
+    num_layers=5,
+    num_heads=2,
+    d_linear_layers=[1000],
 ).to(device)
 
 optimizer = optim.AdamW(
@@ -28,6 +32,10 @@ save_path, _, _ = train(
     optimizer,
     train_loader,
     val_loader,
+    scheduler=optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="max", patience=2, verbose=True, factor=0.7
+    ),
+    custom_loss="contrastive",
     nb_epochs=50,
     device=device,
 )
