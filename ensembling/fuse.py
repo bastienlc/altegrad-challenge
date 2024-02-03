4efd13daac7f0f039e94cfaec5e75923a7d3d081
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-02-03 17:28:40 +0100

    Test other fusing methods

--- ensembling/fuse.py
+++ ensembling/fuse.py
@@ -4,7 +4,43 @@ import numpy as np
 from tqdm import tqdm
 
 
-def condorcet_fuse(similarities, weights=None):
+def rank_norm(similarities):
+    N = similarities.shape[1]
+
+    ranks = N - similarities.argsort(axis=2).argsort(axis=2)
+
+    return 1 - (ranks - 1) / N
+
+
+def strange_norm(similarities):
+    return (similarities - similarities.min(axis=1)[:, None, :]) / (
+        similarities.max(axis=1)[:, None, :] - similarities.min(axis=1)[:, None, :]
+    )
+
+
+def min_max_norm(similarities):
+    return (similarities - similarities.min(axis=2)[:, :, None]) / (
+        similarities.max(axis=2)[:, :, None] - similarities.min(axis=2)[:, :, None]
+    )
+
+
+def sum_norm(similarities):
+    return (similarities - similarities.min(axis=2)[:, :, None]) / (
+        similarities.sum(axis=2)[:, :, None] - similarities.min(axis=2)[:, :, None]
+    )
+
+
+def zmuv_norm(similarities):
+    return (similarities - similarities.mean(axis=2)[:, :, None]) / similarities.std(
+        axis=2
+    )[:, :, None]
+
+
+def max_norm(similarities):
+    return similarities / similarities.max(axis=2)[:, :, None]
+
+
+def condorcet_fuse(similarities, weights=None, norm=strange_norm):
     if weights is None:
         weights = np.ones(similarities.shape[0])
 
@@ -12,11 +48,9 @@ def condorcet_fuse(similarities, weights=None):
     M = similarities.shape[0]
     aggregation = np.zeros((N, N))
 
-    similarities = (similarities - similarities.min(axis=1)[:, None, :]) / (
-        similarities.max(axis=1)[:, None, :] - similarities.min(axis=1)[:, None, :]
-    )
+    similarities = norm(similarities)
 
-    for document in tqdm(range(N)):
+    for document in tqdm(range(N), leave=False):
         other_documents = similarities[:, document, :]
 
         def condorcet_compare(a, b):
@@ -43,29 +77,25 @@ def condorcet_fuse(similarities, weights=None):
     return aggregation
 
 
-def mean_fuse(similarities, weights=None):
+def mean_fuse(similarities, weights=None, norm=strange_norm):
     if weights is None:
         weights = np.ones(similarities.shape[0])
 
-    similarities = (similarities - similarities.min(axis=1)[:, None, :]) / (
-        similarities.max(axis=1)[:, None, :] - similarities.min(axis=1)[:, None, :]
-    )
+    similarities = norm(similarities)
 
     return np.average(similarities, axis=0, weights=weights)
 
 
-def reciprocal_rank_fuse(similarities, weights=None, k=10):
+def reciprocal_rank_fuse(similarities, weights=None, k=10, norm=strange_norm):
     if weights is None:
         weights = np.ones(similarities.shape[0])
 
     N = similarities.shape[1]
     aggregation = np.zeros((N, N))
 
-    similarities = (similarities - similarities.min(axis=1)[:, None, :]) / (
-        similarities.max(axis=1)[:, None, :] - similarities.min(axis=1)[:, None, :]
-    )
+    similarities = norm(similarities)
 
-    for document in tqdm(range(N)):
+    for document in tqdm(range(N), leave=False):
         other_documents = -similarities[:, document, :]
         order = other_documents.argsort(axis=1)
         ranks = order.argsort(axis=1)
@@ -73,3 +103,12 @@ def reciprocal_rank_fuse(similarities, weights=None, k=10):
         aggregation[document, :] = np.average(1 / (ranks + k), axis=0, weights=weights)
 
     return aggregation
+
+
+def prod_fuse(similarities, weights=None, norm=strange_norm):
+    if weights is None:
+        weights = np.ones(similarities.shape[0])
+
+    similarities = norm(similarities)
+
+    return np.prod(similarities * weights[:, None, None], axis=0)
